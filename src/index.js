ab517b86d5e7f4c891f63e25c456193ea353de42
Maxim <maximb+bitbucket@inloop.com>
2019-12-06 15:04:37 +0200

    Change naming, html has become cleaner

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import './js/' // JS - ./js/index.js
-import './scss/main.scss' // SCSS
-import './css/main.css' // CSS (example)
+// JS - ./js/index.js
+import './js/'
 
-window.Vue = require('vue') // Vue.js
-import store from './store'
-Vue.component('example-component', require('./components/Example.vue').default) // Vue components (for use in html)
+// SCSS
+import './scss/main.scss'
+
+// CSS (example)
+import './css/main.css'
 
 // Bootstrap (example)
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
-const app = new Vue({ // Vue init
+// Vue.js
+window.Vue = require('vue')
+import store from './store'
+
+// Vue components (for use in html)
+Vue.component('example-component', require('./components/Example.vue').default)
+
+// Vue init
+const app = new Vue({
   data () {
     return {
-      component: false,
+      showExampleComponent: false,
     }
   },
   store,
