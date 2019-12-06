143e89cb4f51ba966a7e2f81ef0e1c9d63126f08
Maxim <maximb+bitbucket@inloop.com>
2019-12-06 15:02:58 +0200

    Updated static files: favicon.ico, robots.txt, sitemap.xml + comments, styles

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
+import './js/' // JS - ./js/index.js
+import './scss/main.scss' // SCSS
+import './css/main.css' // CSS (example)
 
-// JS - ./js/index.js
-import './js/'
-// SCSS
-import './scss/main.scss'
-// CSS (example)
-import './css/main.css'
+window.Vue = require('vue') // Vue.js
+import store from './store'
+Vue.component('example-component', require('./components/Example.vue').default) // Vue components (for use in html)
 
 // Bootstrap (example)
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
-// import 'vue'
-// import Vue from 'vue'
-window.Vue = require('vue')
-import store from './store'
-
-Vue.component('example-component', require('./components/Example.vue').default)
-
-// With vuex
-const app = new Vue({
+const app = new Vue({ // Vue init
   data () {
     return {
       component: false,
@@ -27,8 +19,3 @@ const app = new Vue({
   store,
   el: '#app'
 })
-
-//* Without vuex and store
-// const app = new Vue({
-//   el: '#app'
-// })
