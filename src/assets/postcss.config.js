de60d93c69f3a61b496787dff156e8b0c3dac5b1
Maxim <maximb+bitbucket@inloop.com>
2019-12-06 16:25:09 +0200

    fonts / postcss comments / test

--- src/assets/postcss.config.js
+++ src/assets/postcss.config.js
@@ -1,18 +1,12 @@
-// autoprefixer - https://github.com/postcss/autoprefixer
-// css-mqpacker - https://github.com/hail2u/node-css-mqpacker
-// cssnano      - https://github.com/hail2u/node-css-mqpacker
-
-// npm install postcss-loader autoprefixer css-mqpacker cssnano --save-dev
-
-module.exports = {
+module.exports = { // npm install postcss-loader autoprefixer css-mqpacker cssnano --save-dev
   plugins: [
-    require('autoprefixer'),
-    require('css-mqpacker'),
-    require('cssnano')({
+    require('autoprefixer'), // добавляет webkit, ms, o и пр. https://github.com/postcss/autoprefixer
+    require('css-mqpacker'), // для media query https://github.com/hail2u/node-css-mqpacker
+    require('cssnano')({ // минифицирует наш код https://github.com/hail2u/node-css-mqpacker
       preset: [
         'default', {
           discardComments: {
-            removeAll: true
+            removeAll: true // просто удаляет комментарии на проде
           }
         }
       ]
