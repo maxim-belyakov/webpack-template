06edf6e5d6cd2103a4bfcae3f82bc889173beaf0
Maxim <maximb+bitbucket@inloop.com>
2019-12-06 16:11:21 +0200

    pages, optimisation

--- src/index.js
+++ src/index.js
@@ -1,30 +1,19 @@
-// JS - ./js/index.js
+// JS
 import './js/'
 
 // SCSS
-import './scss/main.scss'
+import './assets/scss/main.scss'
 
 // CSS (example)
-import './css/main.css'
-
-// Bootstrap (example)
-// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/css/bootstrap.min.css'
+// import './assets/css/main.css'
 
 // Vue.js
 window.Vue = require('vue')
-import store from './store'
 
 // Vue components (for use in html)
 Vue.component('example-component', require('./components/Example.vue').default)
 
 // Vue init
 const app = new Vue({
-  data () {
-    return {
-      showExampleComponent: false,
-    }
-  },
-  store,
   el: '#app'
 })
